b53e6f78a1d73fdfba59337b09191c3b11f2e145
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 23:24:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,8 +27,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["chedder", "gouda", "camembert"]
-    array.find do |cheese|
-    cheese_types.include?(cheese)
-  end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
