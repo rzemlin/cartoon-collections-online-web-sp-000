f61d68faeb6cef15288abeecfe7c4de05150b2cd
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 23:02:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,26 +3,4 @@ def roll_call_dwarves(array)
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
     i += 1 
-  end 
-roll_call_dwarves(array)
-end
-def summon_captain_planet(array)
-  new_array = []
-  i = 0 
-  while i < array.length 
-    new_array << array[i].capitalize + "!"
-    i += 1 
-  end 
-  new_array
-end
-
-def long_planeteer_calls(array) 
-  i = 0 
-  while i < array.length
-    if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
-  end 
-end
+ 
