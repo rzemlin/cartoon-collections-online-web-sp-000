1928492ec470beaa33fb9fd594540e77b483b883
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 22:53:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,20 +7,15 @@ def roll_call_dwarves(array)
 end
 roll_call_dwarves(dwarves)
 
-
-
 def summon_captain_planet(array)
-  nu_array = []
+  new_array = []
   i = 0 
   while i < array.length 
-    nu_array << array[i].capitalize + "!"
+    new_array << array[i].capitalize + "!"
     i += 1 
   end 
-  nu_array
+  new_array
 end
-
-summon_captain_planet(planeteer_calls)
-
 
 def long_planeteer_calls(array) 
   i = 0 
