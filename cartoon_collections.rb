5cdfdd36cca8b5751f9be7343213cece453589d7
Ryan Zemlin <rzemlin@gmail.com>
2021-02-15 23:08:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,3 +6,12 @@ def roll_call_dwarves(array)
   end 
 end
 
+def summon_captain_planet(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+    new_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  new_array
+end
